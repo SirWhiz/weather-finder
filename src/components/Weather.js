c6f21de3893abf8642f99d5c2491f44ae2b0633a
Alvaro <applewhiz@outlook.es>
2019-12-04 14:42:25 +0100

    Finished version 1.0

---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return(
+        <div className="base-results">
+            { props.city && props.country && <p className="cityName">{ props.city }, { props.country }</p> }
+            { props.temperature && <p>Temperature: { props.temperature }C</p> }
+            { props.humidity && <p>Humidity: { props.humidity }%</p> }
+            { props.description && <p>{ props.description } at the moment</p> }
+            { props.error && <p>{ props.error }</p>}
+        </div>
+    );
+}
+
+export default Weather;

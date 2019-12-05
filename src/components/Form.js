4c7e0efd58ab581d7d03c539bd7ed768650698d1
Alvaro <applewhiz@outlook.es>
2019-12-05 12:49:41 +0100

    Fixed CSS and added flag icons

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,13 @@ import React from 'react';
 const Form = (props) => {
 
     return(
-        <form onSubmit={props.getWeather}>
-            <input type="text" name="city" placeholder="City.."></input>
-            <input type="text" name="country" placeholder="Country.."></input>
-            <button>Get Weather</button>
-        </form>
+        <div className="base-form">
+            <form onSubmit={props.getWeather}>
+                <input type="text" name="city" placeholder="City.."></input>
+                <input type="text" name="country" placeholder="Country.."></input>
+                <button>Get Weather</button>
+            </form>
+        </div>
     );
 
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Weather = (props) => {
+
+    var flagUrl = `https://www.countryflags.io/${props.country}/flat/64.png`;
+
     return(
         <div className="base-results">
+            { props.country && <div className="icon-container"><img src={flagUrl} alt="flag icon"></img></div> }
             { props.city && props.country && <p className="cityName">{ props.city }, { props.country }</p> }
             { props.temperature && <p>Temperature: { props.temperature }C</p> }
             { props.humidity && <p>Humidity: { props.humidity }%</p> }

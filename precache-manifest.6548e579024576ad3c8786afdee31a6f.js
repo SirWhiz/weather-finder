a58e2326c4de288e397cf8c1a0edec4860993744
Alvaro <applewhiz@outlook.es>
2019-12-05 13:09:50 +0100

    Updates

---
+++ precache-manifest.6548e579024576ad3c8786afdee31a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f7aca7cced578684a70a287f2a709e",
+    "url": "/weather-finder/index.html"
+  },
+  {
+    "revision": "820cfeb99b424f90eb5b",
+    "url": "/weather-finder/static/css/main.3eb8bf07.chunk.css"
+  },
+  {
+    "revision": "8fe2e5ca53410a516a78",
+    "url": "/weather-finder/static/js/2.b7d10189.chunk.js"
+  },
+  {
+    "revision": "820cfeb99b424f90eb5b",
+    "url": "/weather-finder/static/js/main.b4f57796.chunk.js"
+  },
+  {
+    "revision": "b5a24d3e692558c99c14",
+    "url": "/weather-finder/static/js/runtime-main.82e2479d.js"
+  },
+  {
+    "revision": "eea55f81e090d9a909aa423beab8c47a",
+    "url": "/weather-finder/static/media/bg.eea55f81.jpg"
+  }
+]);
